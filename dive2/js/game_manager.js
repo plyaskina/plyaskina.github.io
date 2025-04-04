db6acbf44c4971e1c6f9f11a9a6deac03954e3fe
plyaskina <137083253+plyaskina@users.noreply.github.com>
2025-04-04 14:27:06 +0300

    Update game_manager.js

--- dive2/js/game_manager.js
+++ dive2/js/game_manager.js
@@ -55,7 +55,7 @@ GameManager.prototype.addStartTiles = function () {
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
-    var value = Math.random() < 0.75 ? this.tileTypes[Math.floor(Math.random() * this.tileTypes.length)] : 1;
+    var value = Math.random() < 0.5 ? this.tileTypes[Math.floor(Math.random() * this.tileTypes.length)] : 1;
     var tile = new Tile(this.grid.randomAvailableCell(), value);
 
     this.grid.insertTile(tile);
