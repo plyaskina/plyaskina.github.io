db67a418fb409024d7b1cb4400fd23be5b0e4a28
plyaskina <137083253+plyaskina@users.noreply.github.com>
2025-08-15 11:33:33 +0300

    Create application.js

---
+++ dive-2x1/js/application.js
@@ -0,0 +1,11 @@
+var game;
+// Wait till the browser is ready to render the game (avoids glitches)
+window.requestAnimationFrame(function () {
+  game = new GameManager({ 'x': 2, 'y': 1 }, KeyboardInputManager, HTMLActuator, LocalScoreManager);
+});
+
+function changeGameMode() {
+  if (game == null)
+    return;
+  game.restart();
+}
