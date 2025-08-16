84fa82d23287e604564b9e0e1b025bf8339610d0
plyaskina <137083253+plyaskina@users.noreply.github.com>
2025-08-16 18:38:30 +0300

    Update grid.js

--- dive-2x1/js/grid.js
+++ dive-2x1/js/grid.js
@@ -8,11 +8,13 @@ function Grid(xsize, ysize) {
 }
 
 // Build a grid of the specified size
-Grid.prototype.build = function () {
-  for (var x = 0; x < this.size; x++) {
-    var row = this.cells[x] = [];
+Grid.prototype.empty = function () {
+  var cells = [];
 
-    for (var y = 0; y < this.size; y++) {
+  for (var x = 0; x < this.xsize; x++) {
+    var row = cells[x] = [];
+
+    for (var y = 0; y < this.ysize; y++) {
       row.push(null);
     }
   }
