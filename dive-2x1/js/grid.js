47eb3310f17f8e61d48d9060d11e943a127a40bd
plyaskina <137083253+plyaskina@users.noreply.github.com>
2025-08-16 18:21:38 +0300

    Update grid.js

--- dive-2x1/js/grid.js
+++ dive-2x1/js/grid.js
@@ -1,39 +1,21 @@
-function Grid(xsize, ysize) {
+function Grid(size) {
   this.xsize = xsize;
   this.ysize = ysize;
+
   this.cells = [];
 
   this.build();
 }
 
 // Build a grid of the specified size
-Grid.prototype.empty = function () {
-  var cells = [];
+Grid.prototype.build = function () {
+  for (var x = 0; x < this.size; x++) {
+    var row = this.cells[x] = [];
 
-  for (var x = 0; x < this.xsize; x++) {
-    var row = cells[x] = [];
-
-    for (var y = 0; y < this.ysize; y++) {
+    for (var y = 0; y < this.size; y++) {
       row.push(null);
     }
   }
-
-  return cells;
-};
-
-Grid.prototype.fromState = function (state) {
-  var cells = [];
-
-  for (var x = 0; x < this.xsize; x++) {
-    var row = cells[x] = [];
-
-    for (var y = 0; y < this.ysize; y++) {
-      var tile = state[x][y];
-      row.push(tile ? new Tile(tile.position, tile.value) : null);
-    }
-  }
-
-  return cells;
 };
 
 // Find the first available random position
@@ -59,8 +41,8 @@ Grid.prototype.availableCells = function () {
 
 // Call callback for every cell
 Grid.prototype.eachCell = function (callback) {
-  for (var x = 0; x < this.xsize; x++) {
-    for (var y = 0; y < this.ysize; y++) {
+  for (var x = 0; x < this.size; x++) {
+    for (var y = 0; y < this.size; y++) {
       callback(x, y, this.cells[x][y]);
     }
   }
@@ -98,24 +80,6 @@ Grid.prototype.removeTile = function (tile) {
 };
 
 Grid.prototype.withinBounds = function (position) {
-  return position.x >= 0 && position.x < this.xsize &&
-         position.y >= 0 && position.y < this.ysize;
-};
-
-Grid.prototype.serialize = function () {
-  var cellState = [];
-
-  for (var x = 0; x < this.xsize; x++) {
-    var row = cellState[x] = [];
-
-    for (var y = 0; y < this.ysize; y++) {
-      row.push(this.cells[x][y] ? this.cells[x][y].serialize() : null);
-    }
-  }
-
-  return {
-    xsize: this.xsize,
-    ysize: this.ysize,
-    cells: cellState
-  };
+  return position.x >= 0 && position.x < this.size &&
+         position.y >= 0 && position.y < this.size;
 };
